b9e6ef25c02db6217603bd67503c02304d8c6610
PengShuai <1499273524@qq.com>
2022-04-19 15:33:22 +0800

    update reload

--- practice/7reload.cpp
+++ practice/7reload.cpp
@@ -4,14 +4,13 @@ class Time{
 private:
     int hh,mm,ss;
 public:
-    Time(int h=0, int m=0, int s=0): hh(h),mm(m),ss(s) {}
+    explicit Time(int h=0, int m=0, int s=0): hh(h),mm(m),ss(s) {}
 
     // 重载 =
     Time& operator = (const Time& t) {
         hh = t.hh;
         mm = t.mm;
         ss = t.ss;
-
         return *this;
     }
 
@@ -32,10 +31,34 @@ public:
         return 0; 
     };
 
-    void ShowTime() {
+    // 友元重载需要参数
+    friend Time operator--(Time &t);
+    friend Time operator--(Time &t, int);
+
+    // 重载二元运算符
+    Time operator+ (Time t) const {
+        return Time(hh + t.hh, mm + t.mm, ss + t.ss);
+    }
+    Time operator- (Time t) const {
+        return Time(hh - t.hh, mm - t.mm, ss - t.ss);
+    }
+
+
+    void ShowTime() const {
         cout << hh << ":" << mm << ":" << ss << endl;
     }
 };
+
+Time operator--(Time &t) {
+    -- t.ss;
+    return t;
+}
+
+Time operator--(Time &t, int) {
+    t.ss --;
+    return t;
+}
+
 int main() {
     Time t1(12,10,11);
     t1.ShowTime();
@@ -60,9 +83,21 @@ int main() {
     cout << "reload [] method:\n";
     int a = t2[1]; 
     int b = t2["abcd"];
-    cout << "a=" << a <<endl;
-    cout << "b=" << b <<endl;
+    cout << "a=" << a << endl;
+    cout << "b=" << b << endl << endl;
 
+    cout << "reload --t and t--:\n";
+    -- t2;
+    t2.ShowTime();
+    t2 --;
+    t2.ShowTime();
+    cout << endl;
+
+    cout << "reload + and -:\n";
+    Time t3 = t1 + t2;
+    t3.ShowTime();
+    Time t4 = t1 - t2;
+    t4.ShowTime();
     return 0;
 }
 
