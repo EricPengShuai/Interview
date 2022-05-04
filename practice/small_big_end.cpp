290506e57b837cc5d5ecc475665b7ca277312333
PengShuai <1499273524@qq.com>
2022-05-04 17:11:42 +0800

    upload small end vs big end

---
+++ practice/small_big_end.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// 联合中只能有一个数据成员可以有值
+	// https://blog.csdn.net/hou09tian/article/details/80816445
+    union {
+        short s;
+        char c[sizeof(short)];
+    } un;
+
+    // https://blog.csdn.net/weixin_43886592/article/details/106336154
+    // 大端（数据高位在低地址）01 02
+    // 小端（数据低位在低地址）02 01
+    un.s = 0x0102;
+
+    if (sizeof(short) == 2) {
+        if (un.c[0] == 1 && un.c[1] == 2) {
+            cout << "大端" << endl;
+        } else if (un.c[1] == 1 && un.c[0] == 2) {
+            cout << "小端" << endl;
+        } else {
+            cout << "unknown" << endl;
+        }
+    } else {
+        cout << "sizeof(short)=" << sizeof(short) << endl;
+    }
+    return 0;
+}
