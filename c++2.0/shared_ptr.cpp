fc597c2bbe089a3e94393d7cd6c121d904f32129
PengShuai <1499273524@qq.com>
2022-08-17 15:33:45 +0800

    update shared_ptr

--- c++2.0/shared_ptr.cpp
+++ c++2.0/shared_ptr.cpp
@@ -20,26 +20,26 @@ int main()
 
 
     auto pointer1 = make_shared<int>(10); 
-    auto pointer12 = pointer1;    // 引用计数 +1 
-    auto pointer13 = pointer1;    // 引用计数 +1 
+    auto pointer2 = pointer1;    // 引用计数 +1 
+    auto pointer3 = pointer1;    // 引用计数 +1 
     int *p = pointer1.get();     // 这样不会增加引用计数 
 
     cout << *p << endl;
     cout << "pointer1.use_count() = " << pointer1.use_count() << endl; // 3 
-    cout << "pointer12.use_count() = " << pointer12.use_count() << endl; // 3 
-    cout << "pointer13.use_count() = " << pointer13.use_count() << endl; // 3
+    cout << "pointer2.use_count() = " << pointer2.use_count() << endl; // 3 
+    cout << "pointer3.use_count() = " << pointer3.use_count() << endl; // 3
 
-    pointer12.reset(); 
-    cout << "reset pointer12:" << endl; 
+    pointer2.reset(); 
+    cout << "reset pointer2:" << endl; 
     cout << "pointer1.use_count() = " << pointer1.use_count() << endl; // 2 
-    cout << "pointer12.use_count() = " << pointer12.use_count() << endl; // 0, pointer12 已 reset 
-    cout << "pointer13.use_count() = " << pointer13.use_count() << endl; // 2 
+    cout << "pointer2.use_count() = " << pointer2.use_count() << endl; // 0, pointer2 已 reset 
+    cout << "pointer3.use_count() = " << pointer3.use_count() << endl; // 2 
 
-    pointer13.reset(); 
-    cout << "reset pointer13:" << endl; 
+    pointer3.reset(); 
+    cout << "reset pointer3:" << endl; 
     cout << "pointer1.use_count() = " << pointer1.use_count() << endl; // 1 
-    cout << "pointer12.use_count() = " << pointer12.use_count() << endl; // 0 
-    cout << "pointer13.use_count() = " << pointer13.use_count() << endl; // 0, pointer13 已 reset
+    cout << "pointer2.use_count() = " << pointer2.use_count() << endl; // 0 
+    cout << "pointer3.use_count() = " << pointer3.use_count() << endl; // 0, pointer3 已 reset
 
     return 0;
 }
