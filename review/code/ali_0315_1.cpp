5e16625c8604a25544834ebc99893648097c6720
PengShuai <1499273524@qq.com>
2023-04-14 16:20:57 +0800

    update ali_0315_1(dfs)

--- review/code/ali_0315_1.cpp
+++ review/code/ali_0315_1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <cstring>
 using namespace std;
 
 int main()
@@ -12,33 +14,44 @@ int main()
         vt[i] = {a, b};
     }
 
-    vector<int> ans;
+    // 统计节点的入度，找到根节点
+    int root = 1;
+    int d[n+1]; memset(d, 0, sizeof d);
+    for (int i = 1; i <= n; ++ i) {
+        if (vt[i].first != -1) d[vt[i].first] += 1;
+        if (vt[i].second != -1) d[vt[i].second] += 1;
+    }
+    for (int i = 1; i <= n; ++ i) {
+        if (d[i] == 0) {
+            root = i;
+            break;
+        }
+    }
+
+    vector<int> dp(n+1, 0);    // dp[i] 表示以 i 为根的子树是多少高度的满二叉树
 
     // dfs，遍历编号为u的节点
-    // 如果是满二叉树，返回其对应的深度，否则返回0
-    function<int(int)> dfs = [&](int u) {
-        // 叶子结点一定是满足满二叉树的
-        if (vt[u].first == -1 && vt[u].second == -1) {
-            ans.push_back(u);
-            return 1;
+    // 如果是满二叉树，dp[i] = 其对应的深度，否则 dp[i] = 0
+    function<void(int)> dfs = [&](int u) {
+        int l = vt[u].first, r = vt[u].second;
+        if (l == -1 && r == -1) {
+            dp[u] = 1;
+            return;
         }
-
-        // 只有一个叶子为空不满足
-        if (vt[u].first == -1 || vt[u].second == -1) {
-            return 0;
+        if (l != -1) dfs(l);
+        if (r != -1) dfs(r);
+        if (l != -1 && r != -1 && dp[l] == dp[r] && dp[l]) {
+            dp[u] = dp[l] + 1;
+        } else {
+            dp[u] = 0;
         }
-
-        // 左右节点不为空递归判断
-        int left = dfs(vt[u].first);
-        int right = dfs(vt[u].second);
-        if (left && right && left == right) {
-            ans.push_back(u);
-            return left + 1;
-        }
-        return 0;
     };
 
-    dfs(1);
-    cout << ans.size() << endl;
+    dfs(root);
+    int ans = 0;
+    for (int i = 1; i <= n; ++ i) {
+        ans += (dp[i] != 0);
+    }
+    cout << ans << endl;
     return 0;
 }
